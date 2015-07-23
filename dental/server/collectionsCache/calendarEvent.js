cf43490e7182caeddafeee05a1db4b2463e933ee
yousour <hengyousour@gmail.com>
2015-07-23 16:03:41 +0700

    cache doctor doc

--- dental/server/collectionsCache/calendarEvent.js
+++ dental/server/collectionsCache/calendarEvent.js
@@ -1,6 +1,4 @@
 // Collection
-Dental.Collection.CalendarEvent.cacheDoc(
-    'register',
-    Dental.Collection.Register,
-    ['registerDate', 'des', 'patientId', '_patient']
-);
+Dental.Collection.CalendarEvent.cacheDoc('register', Dental.Collection.Register, ['registerDate', 'des', 'patientId', '_patient']);
+
+Dental.Collection.CalendarEvent.cacheDoc('doctor', Dental.Collection.Doctor, ['name','gender','startDate','address','telephone']);
