8ff5e7af889f91cdf3d58537f4b327a733099e06
yousour <hengyousour@gmail.com>
2015-07-29 10:40:50 +0700

    create appointment report

---
+++ dental/common/routers/reports/appointmentList.js
@@ -0,0 +1,29 @@
+Router.route('dental/appointmentListReport', function () {
+    this.render('dental_appointmentListReport');
+}, {
+    name: 'dental.appointmentListReport',
+    title: "Appointment List Report",
+    header: {title: 'Appointment Report', sub: '', icon: 'file-text-o'},
+    breadcrumb: {title: 'Appointment Report', parent: 'dental.home'}
+});
+
+Router.route('dental/appointmentListReportGen', function () {
+    // Config layout
+    this.layout('reportLayout', {
+        // Page size: a4, a5, mini
+        // Orientation: portrait, landscape
+        // Font size: fontBody: undefined (10px), bg (12px)
+        data: {
+            pageSize: 'a4',
+            orientation: 'portrait',
+            fontBody: 'bg'
+        }
+    });
+
+    var q = this.params.query;
+    this.render('dental_appointmentListReportGen', {
+        data: function () {
+            return q;
+        }
+    });
+});
