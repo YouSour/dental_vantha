276265668f59d662633245dcfeffad965f4a85a6
Phan Chanra <phan.chanra@gmail.com>
2015-08-03 16:27:39 +0700

    update auto startup orderItem

--- dental/server/startup/orderItem.js
+++ dental/server/startup/orderItem.js
@@ -5,7 +5,9 @@ Meteor.startup(function () {
                 "_id": "000001",
                 "orderCategoryId": "001",
                 "name": "Lyyon",
+				"price":10,
                 "unit": "L",
+				
                 "_orderCategory": {
                     "name": "Water"
                 }
@@ -14,6 +16,7 @@ Meteor.startup(function () {
                 "_id": "000002",
                 "orderCategoryId": "001",
                 "name": "Vital",
+				"price":20,
                 "unit": "L",
                 "_orderCategory": {
                     "name": "Water"
@@ -23,6 +26,7 @@ Meteor.startup(function () {
                 "_id": "000003",
                 "orderCategoryId": "002",
                 "name": "Ezecom",
+				"price":30,
                 "unit": "Mb/s",
                 "_orderCategory": {
                     "name": "Internet"
@@ -30,7 +34,7 @@ Meteor.startup(function () {
             }
         ];
 
-        _.each(data, function (obj) {
+        data.forEach(function (obj) {
             Dental.Collection.OrderItem.insert(obj);
         });
     }
