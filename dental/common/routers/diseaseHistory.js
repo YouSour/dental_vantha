0f251972ad19adf9961d3e39489283e1d1352b17
Yuom Theara <yuom.thera@gmail.com>
2015-08-18 10:58:03 +0700

    update menu label

--- dental/common/routers/diseaseHistory.js
+++ dental/common/routers/diseaseHistory.js
@@ -2,7 +2,7 @@ Router.route('dental/diseaseHistory', function () {
     this.render('dental_diseaseHistory');
 }, {
     name: 'dental.diseaseHistory',
-    title: "Disease History",
-    header: {title: 'Disease History', sub: '', icon: 'list'},
-    breadcrumb: {title: 'Disease History', parent: 'dental.home'}
+    title: "Patient History",
+    header: {title: 'Patient History', sub: '', icon: 'list'},
+    breadcrumb: {title: 'Patient History', parent: 'dental.home'}
 });
