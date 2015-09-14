1717d26c052bc99cf78ac0742438b14e79cc13a0
yousour <hengyousour@gmail.com>
2015-09-14 10:54:05 +0700

    change validation payment & add icon

--- dental/common/collections/payment.js
+++ dental/common/collections/payment.js
@@ -9,66 +9,67 @@ Dental.Collection.Payment = new Mongo.Collection('dental_payment');
  * @type {SimpleSchema}
  */
 Dental.Schema.Payment = new SimpleSchema({
-    patientId:{
-        type: String,
-        label: 'Patient'
-    },
-    registerId: {
-        type: String,
-        label: 'Register ID'
-    },
-    staffId: {
-        type: String,
-        label: 'Staff',
-        autoform: {
-            type: "select2",
-            options: function () {
-                return Dental.List.staff();
-            }
-        }
-    },
-    paymentDate: {
-        type: String,
-        defaultValue: function () {
-            var currentDate = moment(ReactiveMethod.call("currentDate"), 'YYYY-MM-DD HH:mm:ss').format('YYYY-MM-DD H:mm:ss');
-            return currentDate;
-        },
-        //custom: function () {
-        //    if (this.value < moment().format('YYYY-MM-DD')) {
-        //        return "date";
-        //    }
-        //},
-        label: 'Payment Date'
-    },
-    dueAmount: {
-        type: Number,
-        decimal: true,
-        label: 'Due Amount'
-    },
-    paidAmount: {
-        type: Number,
-        decimal: true,
-        min: 0.01,
-        custom: function () {
-            if (this.value > this.field('dueAmount').value) {
-                return "biggerThan";
-            }
-        },
-        label: 'Paid Amount'
-    },
-    balance: {
-        type: Number,
-        decimal: true,
-        label: 'Balance'
-    },
-    status: {
-        type: String,
-        max: 50,
-        optional: true
-    },
-    branchId: {
-        type: String
+  patientId: {
+    type: String,
+    label: 'Patient'
+  },
+  registerId: {
+    type: String,
+    label: 'Register ID'
+  },
+  staffId: {
+    type: String,
+    label: 'Staff',
+    autoform: {
+      type: "select2",
+      options: function() {
+        return Dental.List.staff();
+      }
     }
+  },
+  paymentDate: {
+    type: String,
+    defaultValue: function() {
+      var currentDate = moment(ReactiveMethod.call("currentDate"),
+        'YYYY-MM-DD HH:mm:ss').format('YYYY-MM-DD H:mm:ss');
+      return currentDate;
+    },
+    //custom: function () {
+    //    if (this.value < moment().format('YYYY-MM-DD')) {
+    //        return "date";
+    //    }
+    //},
+    label: 'Payment Date'
+  },
+  dueAmount: {
+    type: Number,
+    decimal: true,
+    label: 'Due Amount'
+  },
+  paidAmount: {
+    type: Number,
+    decimal: true,
+    min: 0.01,
+    custom: function() {
+      if (this.value > this.field('dueAmount').value) {
+        return "greaterThan";
+      }
+    },
+    label: 'Paid Amount'
+  },
+  balance: {
+    type: Number,
+    decimal: true,
+    label: 'Balance'
+  },
+  status: {
+    type: String,
+    max: 50,
+    optional: true
+  },
+  branchId: {
+    type: String
+  }
 });
 
 /**
@@ -80,6 +81,7 @@ Dental.Collection.Payment.attachSchema(Dental.Schema.Payment);
  * Custom Error Message
  */
 SimpleSchema.messages({
-    "biggerThan": "Paid Amount Can't Bigger Than Due Amount",
-    "date": "Payment Date Can't Smaller Than " + moment().format('YYYY-MM-DD H:mm:ss')
+  "greaterThan": "Paid Amount Can't Be Greater Than Due Amount",
+  "date": "Payment Date Can't Smaller Than " + moment().format(
+    'YYYY-MM-DD H:mm:ss')
 });
