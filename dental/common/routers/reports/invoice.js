e291e9f6c668b88ae77e8dd3e19fb7870accf2b4
yousour <hengyousour@gmail.com>
2015-11-27 15:17:35 +0700

    fix font size invoice

--- dental/common/routers/reports/invoice.js
+++ dental/common/routers/reports/invoice.js
@@ -1,31 +1,38 @@
-Router.route('dental/invoiceReport', function () {
-    this.render('dental_invoiceReport');
+Router.route('dental/invoiceReport', function() {
+  this.render('dental_invoiceReport');
 }, {
-    name: 'dental.invoiceReport',
-    title: "Invoice Report",
-    header: {title: 'Invoice Report', sub: '', icon: 'file-text-o'},
-    breadcrumb: {title: 'Invoice Report', parent: 'dental.home'}
+  name: 'dental.invoiceReport',
+  title: "Invoice Report",
+  header: {
+    title: 'Invoice Report',
+    sub: '',
+    icon: 'file-text-o'
+  },
+  breadcrumb: {
+    title: 'Invoice Report',
+    parent: 'dental.home'
+  }
 });
 
-Router.route('dental/invoiceReportGen', function () {
-    // Config layout
-    this.layout('reportLayout', {
-        // Page size: a4, a5, mini
-        // Orientation: portrait, landscape
-        // Font size: fontBody: undefined (10px), bg (12px)
-        data: {
-            pageSize: 'a5',
-            orientation: 'portrait',
-            fontBody: 'bg'
-        }
-    });
+Router.route('dental/invoiceReportGen', function() {
+  // Config layout
+  this.layout('reportLayout', {
+    // Page size: a4, a5, mini
+    // Orientation: portrait, landscape
+    // Font size: fontBody: undefined (10px), bg (12px)
+    data: {
+      pageSize: 'a5',
+      orientation: 'portrait',
+      fontBody: 'bg'
+    }
+  });
 
-    var q = this.params.query;
-    this.render('dental_invoiceReportGen', {
-        data: function () {
-            return q;
-        }
-    });
+  var q = this.params.query;
+  this.render('dental_invoiceReportGen', {
+    data: function() {
+      return q;
+    }
+  });
 }, {
-    name: 'dental.invoiceReportGen'
+  name: 'dental.invoiceReportGen'
 });
