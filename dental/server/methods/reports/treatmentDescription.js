d91d9243cb66451970ff149b4d26fc9fc5988ecb
yousour <hengyousour@gmail.com>
2016-01-25 16:01:14 +0700

    fix interface attachfile & delete patch

--- dental/server/methods/reports/treatmentDescription.js
+++ dental/server/methods/reports/treatmentDescription.js
@@ -7,16 +7,6 @@ Meteor.methods({
       content: []
     };
 
-    /********* Title *********/
-    // var company = Cpanel.Collection.Company.findOne();
-    // data.title = {
-    //   company: company
-    // };
-
-    /********* Header ********/
-    // var registerDoc = Dental.Collection.Register.findOne(self.register);
-    // data.header = registerDoc;
-
     /********** Content & Footer **********/
     var content = [];
     // Get treament
@@ -24,11 +14,33 @@ Meteor.methods({
         _id: self.treatmentId
       })
       .forEach(function(obj) {
+
+        // Patient Photo
+        obj.patientPhoto = "/no-image.png";
+        if (!_.isUndefined(obj._register._patient.photo)) {
+          obj.patientPhoto = Files.findOne(obj._register
+            ._patient
+            .photo).url();
+        }
+
+        //Patient Info
+        obj.patient = obj.patientId + " : " + obj._register._patient.name +
+          " (" + obj._register._patient.gender + ")";
+        //Doctor Info
+        obj.doctor = obj.doctorId + " : " + obj._doctor.name + " (" +
+          obj._doctor.gender + ")";
+
         obj.images = [];
         obj.attachFile.forEach(function(id) {
+          var imageDoc = Files.findOne(id);
           obj.images.push({
-            link: Files.findOne(id).url()
+            name: imageDoc.name(),
+            link: imageDoc.url(),
+            type: imageDoc.type(),
+            size: imageDoc.size() + " Kb",
+            linkDownload: imageDoc.url() + "?download=true"
           });
+          // console.log(obj.images);
         });
         content.push(obj);
       });
