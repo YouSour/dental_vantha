2f5d4aecdc3e3893922837802424b6e933f62462
pisethtieb <pisethsomaly@hotmail.com>
2016-03-01 11:55:06 +0700

    fixed deposit update

--- dental/common/tabulars/deposit.js
+++ dental/common/tabulars/deposit.js
@@ -1,26 +1,27 @@
 Dental.TabularTable.Deposit = new Tabular.Table({
-  name: "dentalDepositList",
-  collection: Dental.Collection.Deposit,
-  columns: [{
-    title: "<i class='fa fa-bars'></i>",
-    tmpl: Meteor.isClient && Template.dental_depositAction
-  }, {
-    data: "depositDate",
-    title: "Deposit Date"
-  }, {
-    data: "amount",
-    title: "Amount",
-    render: function(val, doc, type) {
-      return numeral(val)
-        .format('0,0.00');
-    }
-  }],
-  order: [
-    ["1", "desc"]
-  ],
-  autoWidth: false,
-  columnDefs: [{
-    "width": "12px",
-    "targets": 0
-  }]
+    name: "dentalDepositList",
+    collection: Dental.Collection.Deposit,
+    columns: [{
+        title: "<i class='fa fa-bars'></i>",
+        tmpl: Meteor.isClient && Template.dental_depositAction
+    }, {
+        data: "depositDate",
+        title: "Deposit Date"
+    }, {
+        data: "amount",
+        title: "Amount",
+        render: function (val, doc, type) {
+            return numeral(val)
+                .format('0,0.00');
+        }
+    }],
+    order: [
+        ["1", "desc"]
+    ],
+    extraFields: ['patientId', 'registerId'],
+    autoWidth: false,
+    columnDefs: [{
+        "width": "12px",
+        "targets": 0
+    }]
 });
